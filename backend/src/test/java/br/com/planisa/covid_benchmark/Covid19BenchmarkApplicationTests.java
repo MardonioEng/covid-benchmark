cfc808daf27954b76246334e016eca6b5b4907bf
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2025-02-04 11:39:46 -0300

    Project created

---
+++ backend/src/test/java/br/com/planisa/covid_benchmark/Covid19BenchmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.planisa.covid_benchmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19BenchmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
