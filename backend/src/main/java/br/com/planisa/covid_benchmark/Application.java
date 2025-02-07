c5e3426084ff493c3fcbd8eb8976081c7145c796
Mardonio Rodrigues <mardoniorodrigues11@gmail.com>
2025-02-07 15:22:20 -0300

    feat: Add Swagguer documentation

--- backend/src/main/java/br/com/planisa/covid_benchmark/Application.java
+++ backend/src/main/java/br/com/planisa/covid_benchmark/Application.java
@@ -1,8 +1,17 @@
 package br.com.planisa.covid_benchmark;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+	info = @Info(
+			title = "Planisa Challenge",
+			version = "1.0",
+			description = "Covid-19 Benchmark"
+	)
+)
 @SpringBootApplication
 public class Application {
 
